134fec47313ff2e2bb9784bff13b3f4b83b846f8
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-05-01 10:36:02 +0200

    fixed navBar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,54 @@ function App() {
         <MyNavbar />
       </header>
       <main className="flex-grow-1"></main>
-      <footer></footer>
+      <footer>
+        <section className="mt-5 pb-5 text-secondary pointer">
+          <div>
+            <i className="bi bi-facebook"></i>
+            <i className="bi bi-instagram"></i>
+            <i className="bi bi-twitter"></i>
+            <i className="bi bi-youtube"></i>
+          </div>
+          <div className="container mt-4">
+            <div className="row mb-3">
+              <div className="col-12 col-md-6 col-lg-3">
+                <p className="small mb-2">Audio and Subtitles</p>
+                <p className="small mb-2">Media Center</p>
+                <p className="small mb-2">Privacy</p>
+                <p className="small mb-2">Contact Us</p>
+              </div>
+
+              <div className="col-12 col-md-6 col-lg-3">
+                <p className="small mb-2">Audio Description</p>
+                <p className="small mb-2">Investor Relations</p>
+                <p className="small mb-2">Legal Notices</p>
+              </div>
+
+              <div className="col-12 col-md-6 col-lg-3">
+                <p className="small mb-2">Help Center</p>
+                <p className="small mb-2">Jobs</p>
+                <p className="small mb-2">Cookie Preferences</p>
+              </div>
+
+              <div className="col-12 col-md-6 col-lg-3">
+                <p className="small mb-2">Gift Cards</p>
+                <p className="small mb-2">Terms of Use</p>
+                <p className="small mb-2">Corporate Information</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button className="text-secondary bg-black border border-secondary">
+              Service Code
+            </button>
+          </div>
+          <div>
+            <span className="text-secondary mt-3">
+              © 1997-2026 Netflix, Inc.
+            </span>
+          </div>
+        </section>
+      </footer>
     </div>
   );
 }
--- src/components/footer/MyNavbar.jsx
+++ src/components/footer/MyNavbar.jsx
@@ -1,29 +1,25 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-import LastPartNav from "./LastPartNav";
 
 const MyNavbar = function () {
   return (
-    <Navbar expand="lg" className="bg-trasparent">
+    <Navbar expand="lg" variant="dark" className="bg-transparent">
       <Container fluid>
         <Navbar.Brand href="#">
           <img
             id="logo-netflix"
-            src="../public/netflix_logo.png"
+            src="/netflix_logo.png"
             alt="Netflix Logo"
+            style={{ width: "100px" }}
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+          <Nav className="me-auto my-2 my-lg-0">
             <Nav.Link className="text-white" href="#">
               Home
             </Nav.Link>
             <Nav.Link className="text-white" href="#">
-              Tv Shows
+              TV Shows
             </Nav.Link>
             <Nav.Link className="text-white" href="#">
               Movies
@@ -35,7 +31,26 @@ const MyNavbar = function () {
               My List
             </Nav.Link>
           </Nav>
-          <LastPartNav avatar="../public/avatar.png" />
+
+          <Nav className="d-flex align-items-center flex-row">
+            <Nav.Link className="text-white px-2" href="#">
+              <i className="bi bi-search"></i>
+            </Nav.Link>
+            <Nav.Link className="text-white px-2" href="#">
+              <span className="mb-0">KIDS</span>
+            </Nav.Link>
+            <Nav.Link className="text-white px-2" href="#">
+              <i className="bi bi-bell-fill"></i>
+            </Nav.Link>
+            <Nav.Link className="text-white px-2" href="#">
+              <img
+                id="avatar"
+                src="/avatar.png"
+                alt="Avatar"
+                style={{ width: "30px" }}
+              />
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
