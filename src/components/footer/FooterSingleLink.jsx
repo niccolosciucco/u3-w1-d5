75459817e447cfd67b76d5f81eb26dd445b7ed93
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-05-01 11:44:14 +0200

    fixed footer

--- src/components/footer/FooterSingleLink.jsx
+++ src/components/footer/FooterSingleLink.jsx
@@ -1,7 +1,7 @@
 const FooterSingleLink = function (props) {
-  return props.link.map((link) => {
+  return props.link.map((link, index) => {
     return (
-      <p className="small mb-2" key={link}>
+      <p className="small mb-2 footer-link" key={index}>
         {link}
       </p>
     );
